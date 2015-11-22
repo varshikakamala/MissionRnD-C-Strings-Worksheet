6398d91fcf72048ab86faa59db8a223700411529
varshikakamala <kamalareddy159@gmail.com>
2015-11-22 11:08:50 -0800

    Submit Commit

--- src/KthIndexFromEnd.cpp
+++ src/KthIndexFromEnd.cpp
@@ -12,5 +12,17 @@ NOTES:
 */
 
 char KthIndexFromEnd(char *str, int K) {
+	int i = 0;
+	if (str == "" || str == '\0')
 	return '\0';
+	else
+	{
+		while (str[i] != '\0') i++;
+		if(K < 0 || K>=i)
+			return '\0';
+		else if (K == 0) return str[i-1];
+		else if (K == (i - 1)) return str[0];
+		else if (K == (i - 1) / 2)   return str[i / 2];
+		else			return str[K];
+	}
 }
