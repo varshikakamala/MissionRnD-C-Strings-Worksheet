ed03ddb39811e2f761c60f06e1396f83e63463cd
varshikakamala <kamalareddy159@gmail.com>
2015-12-07 07:30:37 -0800

    Submit Commit

--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -11,6 +11,21 @@ ERROR CASES: Return '\0' for invalid inputs.
 NOTES: Don't create new string.
 */
 
-char removeSpaces(char *str) {
+char * removeSpaces(char *s) {
+	int i, j; 
+	if (s == "" || s == '\0')
 	return '\0';
+		i = 0;
+		j = 0;
+		while (s[i] != '\0')
+		{
+			if (s[i] != ' ')
+			{
+				s[j] = s[i];
+				j++;
+			}
+			i++;
+		}
+		s[j] = '\0';
+		return(s);
 }
